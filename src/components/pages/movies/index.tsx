1a1a39b3d85bf16d632b1ee6a00257f8180e20d5
felikeys <felixnkrumah567@gmail.com>
2024-12-14 19:59:46 +0000

    chore: add a spinner component

--- src/components/pages/movies/index.tsx
+++ src/components/pages/movies/index.tsx
@@ -1,18 +1,23 @@
 import { useGetMoviesQuery } from "@/features/slice/api-slice";
 import MovieCard from "./movie-card";
 import { MovieList } from "./type";
+import Spinner from "../shared/spinner";
 
 const MovieLists = () => {
-    const {data: movieLists} = useGetMoviesQuery({})
+    const {data: movieLists, isLoading} = useGetMoviesQuery({})
 
-    console.log(movieLists);
-  return (
-    <div className="grid grid-cols-4 gap-4">
-        {movieLists?.results.map((movie: MovieList, index: number) => (
-            <MovieCard key={index} movie={movie} />
-        ))}
-    </div>
-  )
+    return isLoading ? (
+        <div className="flex justify-center items-center">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-4">
+          {movieLists?.results.map((movie: MovieList) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      );
+      
 }
 
 export default MovieLists;
